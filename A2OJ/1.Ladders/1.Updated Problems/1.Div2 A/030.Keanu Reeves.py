4471c8f61944809339b7eea980899dc908640f15
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-06-19 05:56:48 +0200

    Done

---
+++ A2OJ/1.Ladders/1.Updated Problems/1.Div2 A/030.Keanu Reeves.py
@@ -0,0 +1,7 @@
+# Problem Name : Keanu Reeves
+# Problem Link : https://codeforces.com/problemset/problem/1189/A
+# Input Operation
+import sys
+n=int(sys.stdin.readline())
+str=sys.stdin.readline()
+# Output Operation
